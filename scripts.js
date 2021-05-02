bcd26a790733ec8c4eecddf4430702555d6a0d44
Pepseso <juna.josef@gmail.com>
2021-05-02 16:04:36 +0200

    Intro to jQuery

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let color = "purple";
+let number = 10;
+let word = "cool";
+if (color === "purple" ) {
+    $(".red").css("background-color", "purple")
+}
+if (number>=100) {
+    $(".yellow").text("whoah, that's a big number.")   
+}else{
+      $(".yellow").text("just a regular number, please.")
+    }
+if (word === "cool") {
+    $(".cyan").text("Power of DOM")
+    
+}else{
+    $(".blue").text("Power of DOM")  
+}    
